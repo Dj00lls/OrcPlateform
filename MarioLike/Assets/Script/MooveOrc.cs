747df1d5649eb75cae1e76dc4f4a424f55e8dc1a
Dj00lls <j.elluin@gmail.com>
2017-05-18 23:39:24 +0200

    Continue level.

--- MarioLike/Assets/Script/MooveOrc.cs
+++ MarioLike/Assets/Script/MooveOrc.cs
@@ -20,9 +20,10 @@ public class MooveOrc : MonoBehaviour {
     void Start () {
         anim = GetComponent<Animator>();
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         float move = Input.GetAxis("Horizontal");
         //rigidbody2D.velocity = new Vector3(move * speed, rigidbody2D.velocity.y);
         anim.SetFloat("speed", Mathf.Abs(move));
@@ -35,26 +36,41 @@ public class MooveOrc : MonoBehaviour {
             Flip();
         }
         this.transform.position += Vector3.right * Input.GetAxis("Horizontal") * Time.deltaTime * speed;
-        
+
         if (Input.GetKeyDown(KeyCode.Z))
         {
             anim.SetTrigger("special");
             Transform t = Instantiate(this.DommagePrefab, this.Spawnpoint.position, this.Spawnpoint.rotation) as Transform;
         }
         if (Input.GetKeyDown(KeyCode.Space) && Physics2D.OverlapCircle(this.transform.position, 0.5f, groundLayer))
+        {
             rigidbody2D.AddForce(new Vector2(0, jump));
+        }
         if (Physics2D.OverlapCircle(this.transform.position, 0.5f, gameOverLayer))
+        {
             Application.LoadLevel(Application.loadedLevel);
-        if(Physics2D.OverlapCircle(this.transform.position, 0.5f, groundLayer))
+        }
+        if (Physics2D.OverlapCircle(this.transform.position, 0.5f, groundLayer))
+        {
             this.transform.position += Vector3.up * Input.GetAxis("Vertical") * Time.deltaTime * speed;
+        }
         if (Physics2D.OverlapCircle(this.transform.position, 0.5f, pickLayer))
         {
-            this.transform.GetComponent<Collider2D>().isTrigger= true;
+            this.transform.GetComponent<Collider2D>().isTrigger = true;
         }
         if (Physics2D.OverlapCircle(this.transform.position, 0.5f, DommageLayer))
+        {
             Application.LoadLevel(Application.loadedLevel);
+        }
         if (Physics2D.OverlapCircle(this.transform.position, 0.5f, BasicEnnemy))
+        {
             Application.LoadLevel(Application.loadedLevel);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
     }
     public void Flip()
     {
